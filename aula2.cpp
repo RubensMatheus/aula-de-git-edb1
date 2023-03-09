6b9350a18ddffcecdeb71d16618ad05682f76afa
RubensMatheus <rubensmatheusvenancio@gmail.com>
2023-03-09 10:26:45 -0300

    função to_string iniciada

--- aula2.cpp
+++ aula2.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
+#include <string>
+#include <sstream>
+
+std::string to_string(int v[], std::size_t){
+    std::ostringstream oss;
+
+}
 
 int main(int argc, char **argv){
 
     int v[]{5, 0, 0, 3, 0, 0, 8 };
     std :: size_t len{7};
 
+    // imprimir vetor na saida padrão
+
+    std::cout << ">>> vetor original: " << to_string(v, len) << '\n';
     return 0;
 }
